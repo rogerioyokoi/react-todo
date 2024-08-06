89c40ee6e26126aa72db8e645b77f34de4d7d021
Rogério Yokoi <rogerio.yokoi@hotmail.com>
2024-08-05 22:35:44 -0300

    fix: fix import ordering on vitest setup

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,9 +1,10 @@
 import '@testing-library/jest-dom';
-import * as TestingLibraryMatchers from '@testing-library/jest-dom/matchers';
 import '@testing-library/jest-dom/vitest';
+import 'vitest-axe/extend-expect';
+
+import * as TestingLibraryMatchers from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
 import { afterEach, expect, vi } from 'vitest';
-import 'vitest-axe/extend-expect';
 import * as AxeMatchers from 'vitest-axe/matchers';
 
 expect.extend(AxeMatchers);
